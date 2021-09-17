891de727ae7e424b90bdcb9c8511dba3859373bc
yyr19921203 <123775172@qq.com>
2021-09-17 14:08:05 +1000

    add

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,6 +2,12 @@ import React, {
 	useState
 } from 'react';
 
+type Greeting = (name: string) => string;
+interface Person {
+	name: string;
+	hungry: boolean
+}
+
 const Home: React.FC = (props) =>{
 	return (
 		<div>
